d70b542c8426cebbfc4cff3ead3d0e22f999012f
John <jwartonick@gmail.com>
2025-07-24 11:10:15 -0400

    Added Code: Added functionality to optimize and train the model

--- chatbot.py
+++ chatbot.py
@@ -26,7 +26,7 @@ test_data = datasets.FashionMNIST(
 
 batch_size = 64 # Batch size of 64: each element
 
-# Create data loader
+    # Create data loader
 train_dataloader = DataLoader(training_data, batch_size=batch_size)
 test_dataloader = DataLoader(test_data, batch_size=batch_size)
 
@@ -35,11 +35,11 @@ for X, y in test_dataloader:
     print(f"Shape of y: {y.shape} {y.dtype}")
     break
 
-# Creating the model
+    # Creating the model
 device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
 print(f"Using {device} device")
 
-# Define the Model
+    # Define the Model
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
@@ -51,7 +51,7 @@ class NeuralNetwork(nn.Module):
             nn.ReLU(),
             nn.Linear(512, 10)
         )
-    
+        
     def forward(self, x):
         x = self.flatten(x)
         logits = self.linear_relu_stack(x)
@@ -59,3 +59,51 @@ class NeuralNetwork(nn.Module):
 
 model = NeuralNetwork().to(device)
 print(model)
+
+    # Optomize model params: need a loss function and an optimizer
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3) # pass in model parameters, and lr: learning rate
+
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        # compute predection error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+
+# Test model's performance against the test dataset to ensure it's learning
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg Loss: {test_loss:>8f} \n")
+
+# Test over iterations (epochs) -> want to see accuracy increase, loss decrease
+epochs = 5
+for t in range(epochs):
+    print(f"Epoch {t+1}\n------------------------------")
+    train(train_dataloader, model, loss_fn, optimizer)
+    test(test_dataloader, model, loss_fn)
+print("Done!")
