d232d98b870c69832a0b46373243c23a94adea25
John <jwartonick@gmail.com>
2025-07-24 10:20:43 -0400

    Added Code: Initialized and defined the model

--- chatbot.py
+++ chatbot.py
@@ -6,6 +6,8 @@ from torchvision.transforms import ToTensor
 
 # print(torch.cuda.is_available()) # cuda is available
 
+# Eventually want to ingest and create a dataset out of pdf docs/texts that i have from humble bundle
+
 # Download training data from open datasets
 training_data = datasets.FashionMNIST(
     root="data",
@@ -33,3 +35,27 @@ for X, y in test_dataloader:
     print(f"Shape of y: {y.shape} {y.dtype}")
     break
 
+# Creating the model
+device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+print(f"Using {device} device")
+
+# Define the Model
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
+    
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+model = NeuralNetwork().to(device)
+print(model)
