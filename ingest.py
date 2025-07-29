ed4a1f6c71411d70783809610cd9359e9e2c03ba
John <jwartonick@gmail.com>
2025-07-29 15:46:51 -0400

    Added code: Added ability to ask user for a path, check the path exists and check there are .pdf files in the directory

--- ingest.py
+++ ingest.py
@@ -1,11 +1,25 @@
 from pypdf import PdfReader
 import sys
+import os
 
-n = len(sys.argv)
-path = ""
+#n = len(sys.argv)
+#path = ""
+#
+#if n > 1:
+#    path = sys.argv[1]
+#
+#print(path)
 
-if n > 1:
-    path = sys.argv[1]
+# get path from user input
+path = input("Enter path to pdf directory: ")
+if os.path.isdir(path) == True:
+    print(f"Path \"{path}\" exists")
+else:
+    print(f"ERROR: path {path} does not exist. Make sure the directory you entered is correct")
 
-print(path)
+# check if .pdf files exist in the directory
+if any(File.endswith(".pdf") for File in os.listdir(path)):
+    print(f"{path} is a valid directory")
+else:
+    print(f"No .pdf files exist in {path}")
 
