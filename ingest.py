5a1920e75981269fa7977d0c4e0f90cb1c2d390a
John <jwartonick@gmail.com>
2025-07-30 10:42:46 -0400

    Added code: Refactored code to optimize a bit, using fitz instead of PDFReader. Simplified check_dir to extract a list of all pdf files in the given directory. changed parse_dir to extract text from the pdf files using fitz.

--- ingest.py
+++ ingest.py
@@ -1,6 +1,7 @@
-from pypdf import PdfReader
+import fitz #PyMuPDF
 import sys
 import os
+from pathlib import Path
 
 #n = len(sys.argv)
 #path = ""
@@ -11,15 +12,35 @@ import os
 #print(path)
 
 # get path from user input
-path = input("Enter path to pdf directory: ")
-if os.path.isdir(path) == True:
-    print(f"Path \"{path}\" exists")
-else:
-    print(f"ERROR: path {path} does not exist. Make sure the directory you entered is correct")
+def check_dir():
+    files = []
+    path = input("Enter path to pdf directory: ")
+    if os.path.isdir(path) == True:
+        dir = Path(path)
+        print(f"Path \"{path}\" exists")
+        files = list(dir.glob("*.pdf"))
+        return files
+    else:
+        print(f"ERROR: path {path} does not exist. Make sure the directory you entered is correct")
 
-# check if .pdf files exist in the directory
-if any(File.endswith(".pdf") for File in os.listdir(path)):
-    print(f"{path} is a valid directory")
-else:
-    print(f"No .pdf files exist in {path}")
+    # check if .pdf files exist in the directory
+    #if any(File.endswith(".pdf") for File in os.listdir(path)):
+    #    print(f"{path} is a valid directory")
+    #    return path
+    #else:
+    #    print(f"No .pdf files exist in {path}")
 
+def parse_dir(files):
+    extracted_text = []
+    for file in files:
+        with fitz.open(file) as doc:
+            full_text = ""
+            print(f"Processing file: {file}")
+            for page in doc:
+                full_text += page.get_text()
+            extracted_text.append(full_text)
+    return extracted_text
+
+path = check_dir()
+text = parse_dir(path)
+print(text)
